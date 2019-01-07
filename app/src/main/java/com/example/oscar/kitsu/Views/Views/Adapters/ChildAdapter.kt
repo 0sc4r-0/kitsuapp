d27ca96b1d595749fe4e33df022170fd1268257a
Oscar <oscar.alexander171@gmail.com>
2019-01-07 16:15:37 -0600

    added onclick to every imageview

--- app/src/main/java/com/example/oscar/kitsu/Views/Views/Adapters/ChildAdapter.kt
+++ app/src/main/java/com/example/oscar/kitsu/Views/Views/Adapters/ChildAdapter.kt
@@ -1,21 +1,25 @@
 package com.example.oscar.kitsu.Views.Views.Adapters
 
 
+
+import android.content.Intent
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import com.example.oscar.kitsu.Views.Views.DetailView
 import com.example.oscar.kitsu.R
 import com.example.oscar.kitsu.Views.Models.ChildModel
 import kotlinx.android.synthetic.main.child_recycler.view.*
 
 class ChildAdapter(private val children : List<ChildModel>)
     : RecyclerView.Adapter<ChildAdapter.ViewHolder>(){
-    val context = this
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v =  LayoutInflater.from(parent.context)
             .inflate(R.layout.child_recycler,parent,false)
@@ -29,12 +33,11 @@ class ChildAdapter(private val children : List<ChildModel>)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val child = children[position]
         holder.imageView.setImageResource(child.image)
         holder.textView.text = child.title
-        holder.imageView.setOnClickListener {this
-            onClick()
-        }
+
     }
 
 
@@ -44,8 +47,5 @@ class ChildAdapter(private val children : List<ChildModel>)
         val imageView: ImageView = itemView.child_imageView
     }
 
-    fun onClick(){
-            Log.i( "","image pressed")
-        //when image pressed create the new fragment
-    }
+
 }
--- app/src/main/java/com/example/oscar/kitsu/Views/Views/MainActivity.kt
+++ app/src/main/java/com/example/oscar/kitsu/Views/Views/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.oscar.kitsu
 
+import android.content.Intent
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+
 import android.widget.LinearLayout
 import com.example.oscar.kitsu.Views.Models.ParentDataFactory
+
 import com.example.oscar.kitsu.Views.Views.Adapters.ParentAdapter
+import com.example.oscar.kitsu.Views.Views.DetailView
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initRecycler()
+
     }
     private fun initRecycler(){
         recyclerView = rv_parent
@@ -24,5 +35,12 @@ class MainActivity : AppCompatActivity() {
             adapter = ParentAdapter(ParentDataFactory.getParents(3))
         }
 
+
     }
+
+    fun onClickDetail(v :View){
+        val intent = Intent(this,DetailView::class.java)
+        startActivity(intent)
+    }
+
 }
