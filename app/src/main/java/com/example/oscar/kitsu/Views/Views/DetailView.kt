7a2b279c8d5978cb199144ab4ed7ed70cb731693
Oscar <oscar.alexander171@gmail.com>
2019-01-09 11:53:27 -0600

    WIP detail view design

--- app/src/main/java/com/example/oscar/kitsu/Views/Views/DetailView.kt
+++ app/src/main/java/com/example/oscar/kitsu/Views/Views/DetailView.kt
@@ -3,6 +3,9 @@ package com.example.oscar.kitsu.Views.Views
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.MovementMethod
+import android.text.method.ScrollingMovementMethod
+import android.widget.TextView
 import com.example.oscar.kitsu.R
 
 import kotlinx.android.synthetic.main.activity_detail_view.*
@@ -12,12 +15,10 @@ class DetailView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_view)
-        setSupportActionBar(toolbar)
+        val textView = findViewById<TextView>(R.id.txtSynopsis)
+        textView.movementMethod = ScrollingMovementMethod.getInstance()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
     }
 
 }
